2b0eb376f54c86fb9f51fdf7fa3a50d29eef418d
Lenny Ramos <lenneyramos@gmail.com>
2026-05-29 00:16:41 -0300

    llego hasta el 6

--- src/billetera/Inversion.java
+++ src/billetera/Inversion.java
@@ -10,17 +10,19 @@ public abstract class Inversion extends Actividad {
 	
     public Inversion(int id, double monto, Cuenta cuentaOrigen, int plazo, boolean esPrecancelable) {
         super(monto, cuentaOrigen, true);
+
+        if (plazo <= 0) {
+            throw new RuntimeException("El plazo debe ser positivo");
+        }
+
         this.id = id;
         this.plazo = plazo;
         this.esPrecancelable = esPrecancelable;
         this.estaPrecancelada = false;
     }
 
-
     public abstract double calcularGanancia();
 
-
-    
     public void cancelar() {
         if (!esPrecancelable) {
             throw new RuntimeException("La inversión no es precancelable");
